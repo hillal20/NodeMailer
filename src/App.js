22a158930e8d0467ef6da3a2fba0caa03d21b3fc
hilal aissani <hilalaissani@gmail.com>
2018-11-06 19:12:17 -0500

    node mailer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentName: "",
+      lastName: ""
+    };
+  }
+
+  eventHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Hello Hilal </h1>
+        <div>
+          student name :
+          <input
+            type="text"
+            placeholder="enter your name"
+            name="studentName"
+            value={this.state.studentName}
+            onChange={this.eventHandler}
+          />
+          Last name :
+          <input
+            type="text"
+            placeholder="enter your last name"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.eventHandler}
+          />
+        </div>
       </div>
     );
   }
